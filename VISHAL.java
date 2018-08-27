fd766647b49a53a74cb166a51bf621449a625e83
tippovishal <42731995+tippovishal@users.noreply.github.com>
2018-08-27 14:00:17 +0530

    Create VISHAL.java

---
+++ VISHAL.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+class VISHAL
+{
+   public static void main(String args[])
+   {
+       System.out.println("enter the character");
+       Scanner sc=new Scanner(System.in);
+       char n=sc.nextChar();
+       
+      if((n=='a')||(n=='e')||(n=='i')||(n=='o')||(n=='u')||(n=='A')||(n=='E')||(n=='I')||(n=='O')||(n=='U'))
+      {
+        System.out.println("is  vowel"+n);
+        }
+        else if(y="w")
+        {
+         System.out.println(" is  not vowel"+n);
+         }
+       }
+     }
+          
