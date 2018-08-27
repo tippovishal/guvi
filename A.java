013d3d90ea5a4cca32760d318e065abbb4614c25
tippovishal <42731995+tippovishal@users.noreply.github.com>
2018-08-27 12:37:36 +0530

    Create A.java

---
+++ A.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class A
+{
+   public static void main(String args[])
+   { 
+      int a=10;
+      int b=-10;
+      if(a>b)
+      {
+        System.out.println("the number is positive");
+        }
+       else
+       {
+        System.out.println("the number is negative");
+        }
+     }
+   }
