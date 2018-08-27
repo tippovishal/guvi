fafa3a7a404993aa495b8d4648dc8d60762c4053
tippovishal <42731995+tippovishal@users.noreply.github.com>
2018-08-27 14:12:21 +0530

    Update VISHAL.java

--- VISHAL.java
+++ VISHAL.java
@@ -2,11 +2,11 @@ import java.io.*;
 import java.util.*;
 class VISHAL
 {
-   public static void main(String args[])
+   public static void main(String[] args)
    {
        System.out.println("enter the character");
        Scanner sc=new Scanner(System.in);
-       char n=sc.nextChar();
+       char n=sc.next()CharAt(0);
        
       if((n=='a')||(n=='e')||(n=='i')||(n=='o')||(n=='u')||(n=='A')||(n=='E')||(n=='I')||(n=='O')||(n=='U'))
       {
