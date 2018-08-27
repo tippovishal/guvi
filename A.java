a02c5febcc51af35ce75fd8c843cb50acdee9040
tippovishal <42731995+tippovishal@users.noreply.github.com>
2018-08-27 12:45:41 +0530

    Update A.java

--- A.java
+++ A.java
@@ -5,12 +5,16 @@ class A
    public static void main(String args[])
    { 
       int a=10;
-      int b=-10;
-      if(a>b)
+ 
+      if(a>0)
       {
         System.out.println("the number is positive");
         }
-       else
+       else if(a==0)
+       {
+        System.out.println("the number is zero");
+        }
+      else 
        {
         System.out.println("the number is negative");
         }
